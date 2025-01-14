cc9e2ea1842ad1df006481813b7b7fabf81292c4
chataeil <ckxodlf12@naver.com>
2025-01-14 14:34:24 +0900

    커밋
    커밋

---
+++ src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -0,0 +1,12 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@EnableJpaAuditing
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+}
